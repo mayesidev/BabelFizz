f990c3a6abd4d9dfbad0288805f820faf8a2b5e4
Isaac Mayes <github@mayesid.com>
2018-09-15 00:21:13 -0500

    Initial VB.Net implementation

--- VBNet/FizzBuzz/FizzBuzz/Program.vb
+++ VBNet/FizzBuzz/FizzBuzz/Program.vb
@@ -1,7 +1,28 @@
-Imports System
+Public Module Program
+    Public Sub Main(args As String())
+        For i As Integer = 1 To 100
+            Dim oStr As String = String.Empty
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+            If i Mod 3 = 0 Then
+                oStr = "Fizz"
+            End If
+
+            If i Mod 5 = 0 Then
+                If oStr <> String.Empty Then
+                    oStr += " "
+                End If
+
+                oStr += "Buzz"
+            End If
+
+            If oStr = Nothing Or oStr = String.Empty Then
+                oStr = i.ToString
+            End If
+
+            Console.WriteLine(oStr)
+        Next
+
+        Console.WriteLine("Please press any key to continue...")
+        Console.ReadKey()
     End Sub
 End Module
