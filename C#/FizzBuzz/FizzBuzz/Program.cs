2f4548360181defe4fe01248a35a737398a277fa
Isaac Mayes <github@mayesid.com>
2018-09-14 23:19:59 -0500

    Initial C# Implementation

--- C#/FizzBuzz/FizzBuzz/Program.cs
+++ C#/FizzBuzz/FizzBuzz/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			for (int i = 1; i <= 100; i++)
+			{
+				string oStr = string.Empty;
+
+				if (i % 3 == 0)
+				{
+					oStr = "Fizz";
+				}
+
+				if (i % 5 == 0)
+				{
+					oStr += string.IsNullOrEmpty(oStr) ? "Buzz" : " Buzz";
+				}
+
+				if (string.IsNullOrEmpty(oStr))
+				{
+					oStr = i.ToString();
+				}
+
+				Console.WriteLine(oStr);
+			}
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
 		}
 	}
 }
