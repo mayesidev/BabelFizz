1321e3da40ba7763c615d3efd9f539bb5fc7a136
Isaac Mayes <github@mayesid.com>
2023-05-01 22:27:10 -0500

    Initial Java implementation

---
+++ Java/FizzBuzz/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            String output = "";
+            if (i % 3 == 0) output += "Fizz";
+            if (i % 5 == 0) output += output.isEmpty() ? "Buzz" : " Buzz";
+            if (output.isEmpty()) output += i;
+            System.out.println(output);
+        }
+    }
+}
